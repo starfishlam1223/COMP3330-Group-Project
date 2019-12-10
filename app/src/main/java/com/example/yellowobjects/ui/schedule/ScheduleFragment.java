6b10d330f1048ea48c262ef295abb3be0cb6f911
benkung101 <44995038+benkung101@users.noreply.github.com>
2019-12-11 01:01:05 +0900

    Add files via upload

--- app/src/main/java/com/example/yellowobjects/ui/schedule/ScheduleFragment.java
+++ app/src/main/java/com/example/yellowobjects/ui/schedule/ScheduleFragment.java
@@ -4,7 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.ImageView;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
@@ -13,6 +15,9 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.yellowobjects.R;
+
+import java.text.DateFormat;
+import java.util.Calendar;
 
 public class ScheduleFragment extends Fragment {
 
@@ -23,13 +28,35 @@ public class ScheduleFragment extends Fragment {
         scheduleViewModel =
                 ViewModelProviders.of(this).get(ScheduleViewModel.class);
         View root = inflater.inflate(R.layout.fragment_schedule, container, false);
-        final TextView textView = root.findViewById(R.id.text_notifications);
-        scheduleViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+        Calendar calendar = Calendar.getInstance();
+        String currentDate = DateFormat.getDateInstance(DateFormat.LONG).format(calendar.getTime());
+        TextView textViewDate = (TextView) root.findViewById(R.id.display_current_date);
+        textViewDate.setText(currentDate);
+        ImageView prevDay = (ImageView) root.findViewById(R.id.previous_day);
+        prevDay.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+
             }
         });
+        ImageView nextDay = (ImageView) root.findViewById(R.id.next_day);
+        prevDay.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+
+            }
+        });
+        ImageView addToSchedule = (ImageView) root.findViewById(R.id.add_to_schedule);
+        prevDay.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+
+            }
+        });
+//        final TextView textView = root.findViewById(R.id.text_notifications);
+//        scheduleViewModel.getText().observe(this, new Observer<String>() {
+//            @Override
+//            public void onChanged(@Nullable String s) {
+//                textView.setText(s);
+//            }
+//        });
         return root;
     }
 }
