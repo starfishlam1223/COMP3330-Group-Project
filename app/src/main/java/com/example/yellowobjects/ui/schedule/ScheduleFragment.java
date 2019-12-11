8cf0955f34ad9a463bc03e0bf9873bba0e8b8074
benkung101 <44995038+benkung101@users.noreply.github.com>
2019-12-11 21:04:09 +0900

    prev day and next day function

--- app/src/main/java/com/example/yellowobjects/ui/schedule/ScheduleFragment.java
+++ app/src/main/java/com/example/yellowobjects/ui/schedule/ScheduleFragment.java
@@ -22,34 +22,47 @@ import java.util.Calendar;
 public class ScheduleFragment extends Fragment {
 
     private ScheduleViewModel scheduleViewModel;
+    private Calendar calendar = Calendar.getInstance();
+    private TextView textViewDate;
+    private String displayDate;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         scheduleViewModel =
                 ViewModelProviders.of(this).get(ScheduleViewModel.class);
         View root = inflater.inflate(R.layout.fragment_schedule, container, false);
-        Calendar calendar = Calendar.getInstance();
-        String currentDate = DateFormat.getDateInstance(DateFormat.LONG).format(calendar.getTime());
-        TextView textViewDate = (TextView) root.findViewById(R.id.display_current_date);
-        textViewDate.setText(currentDate);
+
+        displayDate = DateFormat.getDateInstance(DateFormat.LONG).format(calendar.getTime());
+        textViewDate = (TextView) root.findViewById(R.id.display_current_date);
+        textViewDate.setText(displayDate);
         ImageView prevDay = (ImageView) root.findViewById(R.id.previous_day);
         prevDay.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-
+                previousDay();
             }
         });
         ImageView nextDay = (ImageView) root.findViewById(R.id.next_day);
-        prevDay.setOnClickListener(new View.OnClickListener() {
+        nextDay.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-
+                nextDay();
             }
         });
         ImageView addToSchedule = (ImageView) root.findViewById(R.id.add_to_schedule);
-        prevDay.setOnClickListener(new View.OnClickListener() {
+        addToSchedule.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
 
             }
         });
         return root;
+    }
+    private void previousDay(){
+        calendar.add(Calendar.DAY_OF_MONTH, -1);
+        displayDate = DateFormat.getDateInstance(DateFormat.LONG).format(calendar.getTime());
+        textViewDate.setText(displayDate);
+    }
+    private void nextDay(){
+        calendar.add(Calendar.DAY_OF_MONTH, 1);
+        displayDate = DateFormat.getDateInstance(DateFormat.LONG).format(calendar.getTime());
+        textViewDate.setText(displayDate);
     }
 }
