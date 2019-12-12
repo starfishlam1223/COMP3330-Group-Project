5df93a77f6b3f3262428a9e57cf39facca984fc1
Starfish <starfish.lam.1223@gmail.com>
2019-12-13 00:06:26 +0800

    Partial finished YB quiz

--- app/src/main/java/com/example/yellowobjects/ui/ybmap/Shop.java
+++ app/src/main/java/com/example/yellowobjects/ui/ybmap/Shop.java
@@ -1,14 +1,20 @@
 package com.example.yellowobjects.ui.ybmap;
 
 public class Shop {
+    private int id;
     private String name;
     private boolean isYellow;
     private int imageId;
 
-    public Shop(String name, boolean isYellow, int imageId) {
+    public Shop(int id, String name, boolean isYellow, int imageId) {
+        this.id = id;
         this.name = name;
         this.isYellow = isYellow;
         this.imageId = imageId;
+    }
+
+    public int getId() {
+        return id;
     }
 
     public String getName() {
--- app/src/main/java/com/example/yellowobjects/ui/ybmap/ShopList.java
+++ app/src/main/java/com/example/yellowobjects/ui/ybmap/ShopList.java
@@ -3,6 +3,7 @@ package com.example.yellowobjects.ui.ybmap;
 
 import com.example.yellowobjects.R;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
@@ -12,40 +13,41 @@ public class ShopList {
     int correctCount;
 
     public ShopList() {
+        list = new ArrayList<Shop>();
         currentQuestion = 1;
         correctCount = 0;
 
-        Shop s1 = new Shop("HEYTEA", false, R.drawable.shop1);
-        Shop s2 = new Shop("Super Super Congee & Noodle", false, R.drawable.shop2);
-        Shop s3 = new Shop("Yoshinoya", false, R.drawable.shop3);
-        Shop s4 = new Shop("Cafe De Coral", false, R.drawable.shop4);
-        Shop s5 = new Shop("Starbucks", false, R.drawable.shop5);
-        Shop s6 = new Shop("Pizza Huts", false, R.drawable.shop6);
-        Shop s7 = new Shop("Maxim’s ", false, R.drawable.shop7);
-        Shop s8 = new Shop("Nam Kee Noodle", false, R.drawable.shop8);
-        Shop s9 = new Shop("Genki Sushi", false, R.drawable.shop9);
-        Shop s10 = new Shop("The Great Restaurant", false, R.drawable.shop10);
-        Shop s11 = new Shop("Joint Publishing", false, R.drawable.shop11);
-        Shop s12 = new Shop("Fotomax", false, R.drawable.shop12);
-        Shop s13 = new Shop("Best Mart 360", false, R.drawable.shop13);
-        Shop s14 = new Shop("Okaishi Land", false, R.drawable.shop14);
-        Shop s15 = new Shop("GoGoVan", false, R.drawable.shop15);
+        Shop s1 = new Shop(1, "HEYTEA", false, R.drawable.shop1);
+        Shop s2 = new Shop(2, "Super Super Congee & Noodle", false, R.drawable.shop2);
+        Shop s3 = new Shop(3, "Yoshinoya", false, R.drawable.shop3);
+        Shop s4 = new Shop(4, "Cafe De Coral", false, R.drawable.shop4);
+        Shop s5 = new Shop(5, "Starbucks", false, R.drawable.shop5);
+        Shop s6 = new Shop(6, "Pizza Huts", false, R.drawable.shop6);
+        Shop s7 = new Shop(7, "Maxim’s ", false, R.drawable.shop7);
+        Shop s8 = new Shop(8, "Nam Kee Noodle", false, R.drawable.shop8);
+        Shop s9 = new Shop(9, "Genki Sushi", false, R.drawable.shop9);
+        Shop s10 = new Shop(10, "The Great Restaurant", false, R.drawable.shop10);
+        Shop s11 = new Shop(11, "Joint Publishing", false, R.drawable.shop11);
+        Shop s12 = new Shop(12, "Fotomax", false, R.drawable.shop12);
+        Shop s13 = new Shop(13, "Best Mart 360", false, R.drawable.shop13);
+        Shop s14 = new Shop(14, "Okaishi Land", false, R.drawable.shop14);
+        Shop s15 = new Shop(15, "GoGoVan", false, R.drawable.shop15);
 
-        Shop s16 = new Shop("Odonya Shokudo", true, R.drawable.shop16);
-        Shop s17 = new Shop("Lung Mun Café", true, R.drawable.shop17);
-        Shop s18 = new Shop("CALL4VAN", true, R.drawable.shop18);
-        Shop s19 = new Shop("Cheung Wo Noodles", true, R.drawable.shop19);
-        Shop s20 = new Shop("Kam Cheung Pork Noodle", true, R.drawable.shop20);
-        Shop s21 = new Shop("Station", true, R.drawable.shop21);
-        Shop s22 = new Shop("Katoya", true, R.drawable.shop22);
-        Shop s23 = new Shop("Katsuisen", true, R.drawable.shop23);
-        Shop s24 = new Shop("Kingyo", true, R.drawable.shop24);
-        Shop s25 = new Shop("Zeppelin Hot Dog Shop", true, R.drawable.shop25);
-        Shop s26 = new Shop("Betsutenjin", true, R.drawable.shop26);
-        Shop s27 = new Shop("HEERETEA", true, R.drawable.shop27);
-        Shop s28 = new Shop("Cheap Lab", true, R.drawable.shop28);
-        Shop s29 = new Shop("Queen's Cafe", true, R.drawable.shop29);
-        Shop s30 = new Shop("ChungKee Dessert (Sai Wan)", true, R.drawable.shop30);
+        Shop s16 = new Shop(16, "Odonya Shokudo", true, R.drawable.shop16);
+        Shop s17 = new Shop(17, "Lung Mun Café", true, R.drawable.shop17);
+        Shop s18 = new Shop(18, "CALL4VAN", true, R.drawable.shop18);
+        Shop s19 = new Shop(19, "Cheung Wo Noodles", true, R.drawable.shop19);
+        Shop s20 = new Shop(20, "Kam Cheung Pork Noodle", true, R.drawable.shop20);
+        Shop s21 = new Shop(21, "Station", true, R.drawable.shop21);
+        Shop s22 = new Shop(22, "Katoya", true, R.drawable.shop22);
+        Shop s23 = new Shop(23, "Katsuisen", true, R.drawable.shop23);
+        Shop s24 = new Shop(24, "Kingyo", true, R.drawable.shop24);
+        Shop s25 = new Shop(25, "Zeppelin Hot Dog Shop", true, R.drawable.shop25);
+        Shop s26 = new Shop(26, "Betsutenjin", true, R.drawable.shop26);
+        Shop s27 = new Shop(27, "HEERETEA", true, R.drawable.shop27);
+        Shop s28 = new Shop(28, "Cheap Lab", true, R.drawable.shop28);
+        Shop s29 = new Shop(29, "Queen's Cafe", true, R.drawable.shop29);
+        Shop s30 = new Shop(30, "ChungKee Dessert (Sai Wan)", true, R.drawable.shop30);
 
         list.add(s1);
         list.add(s2);
@@ -87,6 +89,15 @@ public class ShopList {
         list.remove(i);
 
         return selected;
+    }
+
+    public Shop getShop(int i) {
+        for (Shop s : list) {
+            if (i == s.getId()) {
+                return s;
+            }
+        }
+        return null;
     }
 
     public void answer(boolean isCorrect) {
